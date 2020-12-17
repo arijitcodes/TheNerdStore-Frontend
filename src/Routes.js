75a453edf1f57425d33f90bb9fab580f5467f940
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-12-18 01:20:22 +0530

    Add & Mod: Added CSS, Bootstrap. Made some basic changes.

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Components
+import Home from "./core/Home";
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
---
+++ src/core/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-white">Hello Frontend</h1>
+    </div>
+  );
+};
+
+export default Home;
