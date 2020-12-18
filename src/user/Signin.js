c24bb72dbc631984d4b6ae89a27cd60152cd0e28
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-12-19 02:01:44 +0530

    Add: Added User Sing In Methods.

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,24 +1,123 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 
 // Components
 import Base from "../core/Base";
+import { signin, authenticate, isAuthenticated } from "../auth/helper";
 
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    didRedirect: false,
+  });
+
+  const { email, password, error, loading, didRedirect } = values;
+  const { user } = isAuthenticated();
+
+  // Handle form fields value change
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  // On Form Submit
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password })
+      .then((data) => {
+        if (data.err || data.error) {
+          setValues({
+            ...values,
+            error: data.err ? data.err : data.error,
+            loading: false,
+          });
+        } else {
+          authenticate(data, () => {
+            setValues({
+              ...values,
+              didRedirect: true,
+            });
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // Perform User Redirection
+  const performRedirect = () => {
+    // TODO: Make a Redirect
+    if (didRedirect) {
+      if (user && user.role === 1) {
+        return <p>Redirect to Admin</p>;
+      } else {
+        return <p>Redirect to User Dashboard</p>;
+      }
+    }
+
+    if (isAuthenticated()) {
+      return <Redirect to="/" />;
+    }
+  };
+
+  // Loading Message
+  const loadingMessage = () => {
+    return (
+      loading && (
+        <div className="alert alert-info">
+          <h2>Loading...</h2>
+        </div>
+      )
+    );
+  };
+
+  // Error Message
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  // Signin Form
   const signInForm = () => {
     return (
       <div className="row">
         <div className="col-md-6 offset-sm-3 text-left">
-          <form>
+          <form onSubmit={onSubmit}>
             <div className="form-group">
               <label className="text-light">Email</label>
-              <input className="form-control" type="email" name="" />
+              <input
+                onChange={handleChange("email")}
+                value={email}
+                className="form-control"
+                type="email"
+                required
+              />
             </div>
             <div className="form-group">
               <label className="text-light">Password</label>
-              <input className="form-control" type="password" name="" />
+              <input
+                onChange={handleChange("password")}
+                value={password}
+                className="form-control"
+                type="password"
+                required
+              />
             </div>
-            <button className="btn btn-success btn-block">Submit</button>
+            <button type="submit" className="btn btn-success btn-block">
+              Submit
+            </button>
           </form>
         </div>
       </div>
@@ -27,7 +126,11 @@ const Signin = () => {
 
   return (
     <Base title="Sign In page" description="A page for Sign In">
+      {loadingMessage()}
+      {errorMessage()}
       {signInForm()}
+      {performRedirect()}
+      <p className="text-white text-center">{JSON.stringify(values)}</p>
     </Base>
   );
 };
