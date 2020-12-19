a169c55be1d01468258b891c0bbebb3ea811912e
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-12-20 02:37:34 +0530

    Add: Added AdminRoute and PrivateRoute Components for Private Component Router Access.

---
+++ src/auth/helper/AdminRoutes.js
@@ -0,0 +1,27 @@
+import React, { Children } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+// Components and Methods
+import { isAuthenticated } from "./index";
+
+const AdminRoutes = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/signin",
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default AdminRoutes;
