74b9bc4f99b5df718043a6884c7946d65a35e6ad
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-12-27 21:12:35 +0530

    Mod & Update: Moved User Dashboard Items into a new Component 'DashboardItems'.

---
+++ src/user/DashboardItems.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+// Components
+import DashboardItem from "./DashboardItem";
+
+const DashboardItems = () => {
+  return (
+    <div className="row">
+      <DashboardItem
+        title="Profile"
+        body="Your Profile"
+        icon="fas fa-user"
+        link="/profile"
+      />
+
+      <DashboardItem
+        title="Cart"
+        body="Check your Cart"
+        icon="fas fa-shopping-cart"
+        link="/cart"
+      />
+
+      <DashboardItem
+        title="Orders"
+        body="Check your Orders"
+        icon="fas fa-boxes"
+        link="/orders"
+      />
+    </div>
+  );
+};
+
+export default DashboardItems;
--- src/user/UserDashBoard.js
+++ src/user/UserDashBoard.js
@@ -2,35 +2,14 @@ import React from "react";
 
 // Components
 import Base from "../core/Base";
-import DashboardItem from "./DashboardItem";
+import DashboardItems from "./DashboardItems";
 
 const UserDashBoard = () => {
   return (
     <Base title="User Dashboard" description="Dashboard">
       <div className="row">
         <div className="col container">
-          <div className="row">
-            <DashboardItem
-              title="Profile"
-              body="Your Profile"
-              icon="fas fa-user"
-              link="/profile"
-            />
-
-            <DashboardItem
-              title="Cart"
-              body="Check your Cart"
-              icon="fas fa-shopping-cart"
-              link="/cart"
-            />
-
-            <DashboardItem
-              title="Orders"
-              body="Check your Orders"
-              icon="fas fa-boxes"
-              link="/orders"
-            />
-          </div>
+          <DashboardItems />
         </div>
       </div>
     </Base>
